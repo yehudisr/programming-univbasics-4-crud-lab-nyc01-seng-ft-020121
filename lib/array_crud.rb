57dd7d527d2e312aa68ac71432f91907911f4553
yehudis raitman <yehudisr@gmail.com>
2021-01-17 17:12:31 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   my_array = ["tom", "harry"]
-  my_array << "mary"
+  my_array << ("mary")
 end
 
 def add_element_to_start_of_array(array, element)
